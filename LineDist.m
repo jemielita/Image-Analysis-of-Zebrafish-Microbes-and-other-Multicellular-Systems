4166e72cb3e0c0c907ad2d920ac984e310c7f4ad
Matthew Jemielita <matthewjemielita@matthew-jemielitas-computer.local>
2011-10-25 15:09:52 -0700

    LineDist.m code substantially altered. Now it is much easier to use this code as part of a script, since the GUI component has been incorporated into a larger framework.

--- LineDist.m
+++ LineDist.m
@@ -1,14 +1,26 @@
-% A GUI to choose a line along which to measure the average change in
+% SUMMARY: A GUI to choose a line along which to measure the average change in
 % intensity over time
 % GUI will be most applicable to measuring spatial and temporal variation
 % in gut bacterial populations.
-
-
-%LineDist(im)
 %
-%INPUT: im: (optional input) a test image that will be used to find the 
-%region that we will create boxes in to find the average pixel intensity.
+%INPUT:
+%lineDist(): Opens up a GUI that allows the user to pick an image, and then
+%create regions for which to calculate region properties (see USAGE below).
+%The user will be prompted for the number of boxes and save location.
+%lineDist(im): Same as above, but a particular image will be preloaded.
+%lineDist(im, numBoxes): Same as above, but the number of boxes
+%used will be preloaded.
+%lineDist(im, numBoxes, labelMatrix): Same as above, but the label
+%matrix used to find different regions will be preloaded. If this function
+%call is used, no GUI will be opened and the program will return the region
+%properties for all the regions in the label matrix.
 %
+%OUTPUT: [labelMatrix, lineProp ]= lineDist(varargin)
+%          labelMatrix: contains all the regions that have been selected by
+%          the program
+%           lineProp: properties of all the regions that correspond to the
+%           label matrix used.
+% 
 %USAGE:
 %-Click the 'Draw Line' button. The user will then be able to draw a line on
 %the figure. Once the line is placed another line will be created on the
@@ -17,22 +29,84 @@
 %perpendicular to the green line.
 %These two lines define the boundaries of the regions that we will average
 %over.
-
-%Note: 
-%-The code is a little bit buggy. Move the lines around a little bit,
-%even if there initial positioning is perfect, to avoid any problems.
-%-
-%-Enter in the number of boxes that one wants to find averages for.
-%-Press the 'Create Ave. Regions' button. The
-%
 % Created by: Matthew Jemielita, July 19, 2011
+% Substantial revisons: Matthew Jemielita, October 25, 2011
 
 
 
-function [LabelMatrix] = LineDist(varargin)
+function [LabelMatrix_LineDist, lineProp] = lineDist(varargin)
 
 
-fGUI = figure('Name','Time Evolution along a line', 'Menubar','none', ...
+global LabelMatrix_LineDist;
+
+LabelMatrix_LineDist = zeros(1,1); %Label matrix that we'll fill with regions
+%that we're going to calculate properties for.
+
+%First parse the input variables
+
+
+if(nargin==3)
+    LabelMatrix_LineDist = varargin{3};
+elseif(nargin==0)
+    global im;  
+    LineDistGUI(); %Run the program with the GUI
+elseif(nargin==1)
+    LineDistGUI(varargin{1});
+elseif(nargin==2)
+    LineDistGUI(varargin{1},varargin{2});
+else
+    disp('This program takes at most three arguments!');
+    return
+    
+end
+
+%While the GUI is open, pause this program
+while(~isempty(findobj('Tag', 'LineDistGUI')))
+    pause(0.5);
+    
+end
+
+if(nargin==2)
+    outputIm  = varargin{1};
+else
+    outputIm = im;
+end
+%Calculate the region properties for the selected image
+lineProp = regionprops(LabelMatrix_LineDist, outputIm, 'MeanIntensity', 'Centroid');
+
+end
+
+%This GUI provides a way for the user to manually select the regions of
+%interest to calculate a line distribution.
+function LineDistGUI(varargin)
+
+
+%Defining globabl variables
+
+slope = 0; %slope of the first line used to find the region of interest
+theta = 0; %arctan(slope)
+
+xpos= [0 0]; %Position of first line
+ypos= [ 0 0];
+
+xpos2 = [0 0]; %Position of second line
+ypos2 = [0 0];
+
+region.position = [0 0;0 0; 0 0; 0 0];
+region.mean = 0;
+
+hLine1 = {};
+hLine2 = {};
+
+
+numBoxes = -1;
+
+global LabelMatrix_LineDist;
+
+LabelMatrix_LineDist = zeros(1,1); 
+
+
+fGUI = figure('Tag', 'LineDistGUI', 'Name','Time Evolution along a line', 'Menubar','none', ...
     'Visible','off','Position',[100,100,1400,800]);
 %Create the axis on which the image will be displayed.
 axeshandle = axes('Position', [0.05 0.25 0.6 0.6], 'Parent', fGUI);
@@ -60,14 +134,6 @@ hCreateHist   = uicontrol('Parent', hManipulate,'Style','pushbutton',...
     'Position',[0.1, 1-3*0.16,0.4,0.15],...
     'Callback',{@createHist});
 
-hSaveHist = uicontrol('Parent', hManipulate, 'Style', 'pushbutton',...
-    'String', 'Save Ave. Regions', 'Units', 'normalized',...
-    'Position',[0.1, 1-5*0.16,0.4,0.15],...
-    'Callback', {@saveHist});
-hCalcHist = uicontrol('Parent', hManipulate,'Style','pushbutton',...
-    'String','Calc. Region Props. ','Units', 'normalized',...
-    'Position',[0.1, 1-7*0.16,0.4,0.15],...
-    'Callback',{@calcProp});
 %Set the number of boxes to use in the averaging
 hSetNumBoxesText = uicontrol('Parent', hManipulate,...
     'Style','text','Units', 'normalized',...
@@ -79,33 +145,18 @@ hSetNumBoxes= uicontrol('Parent', hManipulate, 'style','edit',...
                  'position',[0.30 1-4*0.16 0.4 .10],...
                  'fontsize',8,...
                  'string','Number of Boxes', 'Callback',{@setNumBoxes});
-             
-
-%Defining globabl variables
-
-slope = 0; %slope of the first line used to find the region of interest
-theta = 0; %arctan(slope)
-
-xpos= [0 0]; %Position of first line
-ypos= [ 0 0];
-
-xpos2 = [0 0]; %Position of second line
-ypos2 = [0 0];
-
-region.position = [0 0;0 0; 0 0; 0 0];
-region.mean = 0;
-
-hLine1 = {};
-hLine2 = {};
-numBoxes = -1;
-
-
-LabelMatrix = zeros(1,1); %Label matrix that we'll fill with regions
-%that we're going to calculate properties for.
+  
 set(fGUI, 'Visible', 'on')
 
 %See if the user has provided an image, if so display it.
-if(nargin==1)
+%If the user doesn't provide an image then make im global so that we can
+%pass it to the master function
+
+if(nargin==0)
+   global im;
+end
+
+if(nargin==1 ||nargin==2)
     im = varargin{1};
     im = mat2gray(im);
     
@@ -113,9 +164,14 @@ if(nargin==1)
     %into the code
     hIm = imshow(im, [], 'Parent', axeshandle);
     
-    LabelMatrix = zeros(size(im));
+    LabelMatrix_LineDist = zeros(size(im));
+end
+
+if(nargin==2)
+    numBoxes = varargin{2};
     
-elseif(nargin>1)
+    set(hSetNumBoxes, 'String', num2str(numBoxes));
+elseif(nargin>2)
     disp('Too many input parameters! The user only has to provide an image.');
     
 end
@@ -129,14 +185,17 @@ end
         %User selects the image to use
         [filename, pathname, filterindex] = uigetfile('.tif', 'Select an Image' );
         
-        imInput = strcat(pathname, filesep, filename);
-        im = imread(imInput);
+        if(filename~= 0)
+            imInput = strcat(pathname, filesep, filename);
+            im = imread(imInput);
+            
+            %Display the image of interest, the intensity scale is hardcoded
+            %into the code
+            hIm = imshow(im, [0 500], 'Parent', axeshandle);
+            
+            LabelMatrix_LineDist = zeros(size(im));
+        end
         
-        %Display the image of interest, the intensity scale is hardcoded
-        %into the code
-        hIm = imshow(im, [0 500], 'Parent', axeshandle);
-        
-        LabelMatrix = zeros(size(im));
     end
 
     function drawLine(source,eventdata)
@@ -155,8 +214,10 @@ end
         xpos(2) = initPos(2,1);
         ypos(1) = initPos(1,2);
         ypos(2) = initPos(2,2);
-        
-        
+             
+        %Calculate the initial theta
+        slope = (ypos(2) - ypos(1))/(xpos(2) - xpos(1));
+        theta = atan(slope);
         
         xpos2 = xpos + cos(theta) * 80;
         ypos2 = ypos -sin(theta)*80;
@@ -202,8 +263,7 @@ end
       constrainedPos(:,2) = ypos - cos(theta)*dist;
      
       xpos2 = constrainedPos(:,1);
-      ypos2 = constrainedPos(:,2);
-      
+      ypos2 = constrainedPos(:,2);     
     end
 
 
@@ -212,8 +272,7 @@ end
     function setNumBoxes(source, eventdata)
       
         user_string = get(source,'String');
-        numBoxes = str2num(user_string);
-    
+        numBoxes = str2num(user_string);   
         
     end
 %Create the necessary masks to produce a histogram over the regions we're
@@ -249,135 +308,47 @@ end
        %of the algorithm.
        regionMask = zeros(size(im));
        
-        for i=1: numBoxes
-
-        xPosLast(1)  = xPosFirst(1) + step*cos(theta);
-        xPosLast(2)  = xPosFirst(2) + step*cos(theta);
-        
-        yPosLast(1) = yPosFirst(1) + step*sin(theta);
-        yPosLast(2) = yPosFirst(2) + step*sin(theta);
-        
-        %Updating the structure for this region
-        region(i).position = [xPosFirst(1) yPosFirst(1) ; xPosLast(1) yPosLast(1);...
-            xPosLast(2) yPosLast(2); xPosFirst(2) yPosFirst(2)];
-        
-  
-        %Creating the label matrix and displaying the regions that we've
-        %created.
-        hPoly = impoly(axeshandle, region(i).position);
-        BW = createMask(hPoly);
-    
-        %Update the label matrix.
-        LabelMatrix(BW==1) = i;
-        
-        %Finding the edges of this mask.
-        %Note: The edges of the region seem somewhat choppy because of the
-        %edge filter used. To make a solid boundary we'd have to rewrite
-        %this a bit.
-        
-        BWEdge = edge(BW, 'canny');
-        
-        regionMask(BWEdge) = 1;
-        
-        %Overlay this mask with the original image
-        imRegion = imoverlay(im, logical(regionMask), [1 0 0]);
-        
-        imshow(imRegion, 'Parent', axeshandle);
-        drawnow;
-        
-        xPosFirst = xPosLast;
-        yPosFirst = yPosLast;
+       for i=1: numBoxes       
+           xPosLast(1)  = xPosFirst(1) + step*cos(theta);
+           xPosLast(2)  = xPosFirst(2) + step*cos(theta);
+           
+           yPosLast(1) = yPosFirst(1) + step*sin(theta);
+           yPosLast(2) = yPosFirst(2) + step*sin(theta);
+           
+           %Updating the structure for this region
+           region(i).position = [xPosFirst(1) yPosFirst(1) ; xPosLast(1) yPosLast(1);...
+               xPosLast(2) yPosLast(2); xPosFirst(2) yPosFirst(2)];
+           
+           
+           %Creating the label matrix and displaying the regions that we've
+           %created.
+           hPoly = impoly(axeshandle, region(i).position);
+           BW = createMask(hPoly);
+           
+           %Update the label matrix.
+           LabelMatrix_LineDist(BW==1) = i;
+           
+           %Finding the edges of this mask.
+           %Note: The edges of the region seem somewhat choppy because of the
+           %edge filter used. To make a solid boundary we'd have to rewrite
+           %this a bit.
+           
+           BWEdge = edge(BW, 'canny');
+           
+           regionMask(BWEdge) = 1;
+           
+           %Overlay this mask with the original image
+           imRegion = imoverlay(im, logical(regionMask), [1 0 0]);
+           
+           imshow(imRegion, 'Parent', axeshandle);
+           drawnow;
+           
+           xPosFirst = xPosLast;
+           yPosFirst = yPosLast;
            
        end
        
        disp('Regions created!');
     end
 
-%Calculate the average in the region of interest
-    function calcProp(source,eventdata)
-        disp('Calculating regions properies...');
-        
-        %Get the directory where the scans and images are stored
-        rootDir = uigetdir;
-        
-        %Get the scan and image range to collect data for, along with the
-        %root name of the scan folder and images
-        
-        prompt = {'Enter base name of scan folders: ', ...
-            'Enter first scan to process: ', 'Enter last scan to process: ', ...
-            'Enter base name of image files: ', 'Enter first image to process',...
-            'Enter last image to process'};
-        dlg_title = 'Scan and Image information'; num_lines= 1;
-        def     = {'scan_','1','', 'image', '1', ''};  % default values
-        answer  = inputdlg(prompt,dlg_title,num_lines,def);
-        
-        %Directory information
-        dirNameRoot = char(answer(1));
-        dirFirst = str2double(answer(2));
-        dirLast = str2double(answer(3));
-        
-        %Image information
-        fileNameRoot = char(answer(4));
-        fileFirst = str2double(answer(5));
-        fileLast = str2double(answer(6));
-
-        %Going to be clumsy right now, just to get something on paper, this
-        %loop should be hard coded for the 
-        
-        %for i =1:numBoxes
-         %  region(i).mean = zeros(130,1);
-           
-        %end
-        
-        %Get a location to save the data to
-        
-        [fileName, pathName, filterIndex] =...
-            uiputfile('*.mat' ,'Choose/ create a file to write data to');
-        
-        saveFile  = strcat(pathName, fileName);
-        
-        %Save information about the regions used in the calculations
-        save(saveFile, 'region');
-        
-        for j =dirFirst:dirLast
-        
-          dirname = strcat(rootDir, filesep,dirNameRoot, num2str(j));
-          cd(dirname);
-          data = zeros(130,numBoxes);
-         
-          for k=fileFirst:fileLast
-              % for k =fileFirst:fileLast
-              k
-              
-              filename = strcat(fileNameRoot, num2str(k), '.tif');
-              imIn = imread(filename);
-              
-              %Calculating the region properties
-              b = regionprops(LabelMatrix, imIn, 'MeanIntensity');
-              %Clumsily turning this into an array
-              
-              b
-              for i= 1:numBoxes
-                  data(k,i) = b(i).MeanIntensity;
-                  data(k,i)
-              end
-              
-              
-          end
-          
-          outData(j).Data = data;
-          
-          save(saveFile, 'outData', '-append')
-          
-  
-          
-          
-        %save(saveFile, 'data');
-          j
-        end
-        saveFile = 'LineData.mat';
-        save(saveFile, 'outData');
-       
-        disp('Region properties calculated!');
-    end
 end
