81add1241b2edc200da2694a2f568bad61b1a242
Matthew Jemielita <matthewjemielita@matthew-jemielitas-computer.local>
2011-12-12 16:47:12 -0800

    temporary change to curveMask to increase width of boxes. Need to make code more robust to avoid this in future.

--- centerGut/curveMask.m
+++ centerGut/curveMask.m
@@ -75,13 +75,15 @@ end
                 xI = x+1;
                 yI = y+2;
                 
+                %The length of these lines should be long enough to
+                %intersect the gut...doesn't seem to be the case right now.
                 Orth = [xI yI] - ((x*xI + yI*y)/(x^2 +y^2))*[x y];
                 
-                xVal(1,:) = xx(i)+ Orth(1)*(100)*[-1, 1];
-                yVal(1,:) = yy(i)+ Orth(2)*(100)*[-1,1];
+                xVal(1,:) = xx(i)+ Orth(1)*(500)*[-1, 1];
+                yVal(1,:) = yy(i)+ Orth(2)*(500)*[-1,1];
                 
-                xVal(2,:) = xx(i+1)+ Orth(1)*(100)*[-1, 1];
-                yVal(2,:) = yy(i+1)+ Orth(2)*(100)*[-1,1];
+                xVal(2,:) = xx(i+1)+ Orth(1)*(500)*[-1, 1];
+                yVal(2,:) = yy(i+1)+ Orth(2)*(500)*[-1,1];
                 
                 
             case 'curved'
@@ -92,8 +94,8 @@ end
                 
                 Orth = [xI yI] - ((x*xI + yI*y)/(x^2 +y^2))*[x y];
                 
-                xVal(1,:) = xx(i)+ Orth(1)*(100)*[-1, 1];
-                yVal(1,:) = yy(i)+ Orth(2)*(100)*[-1,1];
+                xVal(1,:) = xx(i)+ Orth(1)*(500)*[-1, 1];
+                yVal(1,:) = yy(i)+ Orth(2)*(500)*[-1,1];
                 
                 
                 x = xx(i+1)-xx(i);
@@ -104,8 +106,8 @@ end
                 Orth = [xI yI] - ((x*xI + yI*y)/(x^2 +y^2))*[x y];
                 
                 
-                xVal(2,:) = xx(i+1)+ Orth(1)*(100)*[-1, 1];
-                yVal(2,:) = yy(i+1)+ Orth(2)*(100)*[-1,1];
+                xVal(2,:) = xx(i+1)+ Orth(1)*(500)*[-1, 1];
+                yVal(2,:) = yy(i+1)+ Orth(2)*(500)*[-1,1];
                 
                 
         end
