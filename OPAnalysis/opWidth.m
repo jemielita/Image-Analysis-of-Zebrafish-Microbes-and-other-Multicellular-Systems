ce4668f9b14f0ff62d93574ad82f635013653c21
lsm_Analysis_mlj <mlj@uoregon.edu>
2012-03-31 19:56:22 -0700

    opWidth.m now makes it possible to find the average width. TiffS upgraded a bit to make things go more smoothly. Need to do more post-processing of markers.

--- OPAnalysis/opWidth.m
+++ OPAnalysis/opWidth.m
@@ -1,5 +1,29 @@
-function [convexPt, linePt] = opWidth(imT, pos)
+%[convexPt, linePt, imT] = opWidth(imT)
+%
+%INPUT: imT: thresholded 3D image giving the extent of an opercle. Image
+%stack should be binary. 1: opercle, 0: not opercle.
+%
+%OUTPUT: convexPt: gives points defining the convex hull at given points
+%along the opercle. Stored as a cell array.
+%        linePt: gives points that define the principal axis through the
+%        opercle.
+%        imT: returns the thresholded image of the opercle, but cropped
+%        down so that the image stack is only as large as the opercle.
+%
+%DETAILS: For a given 3D segmented image of an opercle calculate the
+%principal axis of the image using principal component analysis. Since the
+%opercle is longer than it is wide, the long axis of the opercle will be
+%given by the first principal component. Along this line find points of the
+%opercle that are perpendicular to the principal axis. Calculate the convex
+%hull of these points and return them. This will give the perimeter of the
+%opercle at different points along its length.
+%
+%AUTHOR: Matthew Jemielita
+
+function [convexPt, linePt] = opWidth(imT)
 %Rescale the thresholded image so that it is only as large as necessary
+imT = double(imT>0);
+
 xy = sum(imT,3);
 
 xR = sum(xy,1);
@@ -21,8 +45,6 @@ zMax = find(zR, 1, 'last');
 
 %Redefining imT to be the smallest box surrounding the opercle.
 imT = imT( yMin:yMax, xMin:xMax, zMin:zMax);
-
-
 
 %Find the perimeter of these regions
 imPerim = zeros(size(imT));
@@ -47,7 +69,6 @@ hold on
 %the convex hull of the plane perpendicular to this line
 ind = find(imT==1);
 
-
 [x, y,z] = ind2sub(size(imT), ind);
 %rescaling the z axis to account for the spacing
 z = (1/0.1625)*z;
@@ -56,7 +77,6 @@ X = cat(2, x,y,z);
 
 %Get the principal components
 [coeff,score,roots] = princomp(X);
-
 %We'll find a plane normal to the principal axis
 basis = coeff(:,2:3);
 
@@ -67,14 +87,12 @@ meanX = mean(X,1);
 Xfit = repmat(meanX,n,1) + score(:,1:2)*coeff(:,1:2)';
 residuals = X - Xfit;
 
-
 %Getting points along the principal axis
 dirVect = coeff(:,1);
 t = [min(score(:,1))-.2, max(score(:,1))+.2];
 endpts = [meanX + t(1)*dirVect'; meanX + t(2)*dirVect'];
 
 %resampling at a space of one pixel-maybe overkill
-
 plot3(endpts(:,1), endpts(:,2), endpts(:,3), 'k-');
 
 %Parameterizing curve in terms of arc length
@@ -111,7 +129,6 @@ figure;
 fprintf(2, 'Calculating the convex hull perpendicular to the principal axis');
 fprintf(2, '\n');
 for lineNum = 1:size(linePt,1)
-    
     lineVal = linePt(lineNum,:);
     zgrid = (1/normal(3)) .* (lineVal*normal - (xgrid.*normal(1) + ygrid.*normal(2)));
     
@@ -125,24 +142,34 @@ for lineNum = 1:size(linePt,1)
     planePt = round(planePt);
     interPtIn = ismember(planePt, perimVal,'rows');
     
-    gridPlane(:) = 0;
-    gridPlane(interPtIn) = 1;
-    imshow(gridPlane);
-    title(lineNum);
-    drawnow;
+    %Find the coordinates of these points in the original coordinate system
+    valOrig = cat(2, xgrid(interPtIn), ygrid(interPtIn), zgrid(interPtIn));
+    %Transforming to the coordinate system of the plane perpendicular to
+    %the principal axis.
+    val = basis'*valOrig';
     
-    interPt = planePt(interPtIn,:);
-   [xyCon, zCon] = ind2sub(size(xgrid), interPtIn);
-   
-   
-   if(length(unique(zCon))>1)
-       k = convhull(xyCon, zCon);
-       xyCon = xyCon(k); zCon = zCon(k);
-       convexPt{lineNum-1} = [xyCon, zCon];
-   end
+    %DOUBLE CHECK THAT THIS IS THE CORRECT TRANSFORMATION!!!
+    %I believe it is. If you run the command below-finding the coordinate
+    %of all these points along the normal all the values are the same. This
+    %is what you would expect.
+    %val2 = normal'*valOrig';
+    %
+    
+    
+   if(size(val,2)>1)
+       k = convhull(val(1,:), val(2,:));
+       valCon = val(:,k);
+       convexPt{lineNum-1} = valCon;
        
-    %if(size(interPt,1)==0)
-    %delete(hP1), delete(hP2)
+       %Convex hull is what one expects.
+%        plot(val(1,:), val(2,:), '*');
+%        hold on
+%        plot(valCon(1,:), valCon(2,:), '--rs');
+%        pause
+%        close all
+       
+   end
+   
     if(exist('hP1'))
         delete(hP1)
     end
@@ -152,137 +179,8 @@ for lineNum = 1:size(linePt,1)
     
     fprintf(2, '.');
     
-%     hP1 = plot3(interPt(:,1), interPt(:,2), interPt(:,3), '-rs','MarkerFaceColor', 'g',...
-%         'MarkerSize', 5);
-%     hP2 = plot3(lineVal(:,1), lineVal(:,2), lineVal(:,3), '-s','MarkerSize', 5, 'MarkerFaceColor', 'r');
-%     %pause
-%     
-% %     else
-% %        set(hP1, 'xData', interPt(:,1));set(hP1, 'Data', interPt(:,2)); 
-% %        set(hP1, 'yData', interPt(:,3)); 
-% %        
-% %         set(hP2, 'xData', lineVal(:,1));set(hP2, 'yData', lineVal(:,2)); 
-% %        set(hP2, 'zData', lineVal(:,3)); 
-% %        
-% %        
-% %         
-% %     end
-%     title(lineNum)
-%     drawnow;
-%     b =0;
-    
-    %Now finding the location of these 
-    
 end
 fprintf(2, '\n');
-% 
-% 
-% %Rescaling pos
-% pos(1,1) = pos(1,1) - xMin;
-% pos(2,1) = pos(2,1) - xMin;
-% pos(1,2) = pos(1,2) - yMin;
-% pos(2,2) = pos(2,2) - yMin;
-% 
-% %Along the length of the gut, calculate the projection
-% xx = pos(1,1) + (pos(2,1)-pos(1,1))*(1:500)/500;
-% 
-% yy = pos(1,2) + (pos(2,2)-pos(1,2))*(1:500)/500;
-%     
-% 
-% %Parameterizing curve in terms of arc length
-% t = cumsum(sqrt([0,diff(xx(:)')].^2 + [0,diff(yy(:)')].^2));
-% 
-% %Resampling so that y is sampled at spacings of one pixel-there's no need
-% %to sample any finer.
-% 
-% xx = spline(t, xx, t);
-% yy = spline(t, yy, t);
-% 
-% stepSize = 1;
-% xx = interp1(t, xx, min(t):stepSize:max(t), 'spline', 'extrap');
-% yy = interp1(t, yy, min(t):stepSize:max(t), 'spline', 'extrap');
-% 
-% yMin = 1;
-% xMin = 1;
-% 
-% yMax = size(imT,1);
-% xMax = size(imT,2);
-% 
-% figure; imshow(sum(imPerim,3));
-% hold on
-%     
-% fprintf(2,'Getting convex hull down the width of OP:\n');
-% 
-% perimW = zeros(length(xx)-1);
-% 
-% 
-% lineIm = zeros(size(imT,1), size(imT,2)); %For getting a mask for the line
-% 
-% for lineNum=2:length(xx)
-%     
-%     x = xx(lineNum)-xx(lineNum-1);
-%     y = yy(lineNum)-yy(lineNum-1);
-%     xI = x+1;
-%     yI = y+2;
-%     
-%     %Line should be long enough to span the entire gut...Far more than that
-%     %right now.
-%     Orth = [xI yI] - ((x*xI + yI*y)/(x^2 +y^2))*[x y];    
-%     xVal = xx(lineNum)+ Orth(1)*[-500:1:500];
-%     yVal = yy(lineNum)+ Orth(2)*[-500:1:500];
-%     
-%     %Get rid of elements of xVal and yVal outside the image size
-%     index = find(round(xVal)>xMax |round(xVal)<xMin);
-%     xVal(index) = [];
-%     yVal(index) = [];
-%     
-%     index = find(round(yVal)>yMax |round(yVal)<yMin);
-%     xVal(index) = [];
-%     yVal(index) =[];
-%     
-%     indexLine = cat(2, round(xVal)', round(yVal)');
-%    
-%     onLine = sub2ind([size(imT,1), size(imT,2)], indexLine(:,2), indexLine(:,1));
-%     lineIm(:) = 0;
-%     lineIm(onLine) =1;
-%     
-%     %See where the line intersects with the perimeter of the opercle.
-%     overlapIm = imPerim.*repmat(lineIm, [1,1,size(imT,3)]);
-%     
-%     %Get the location of the pixels that make up the boundary
-%     ind = find(overlapIm==1);
-%     [xPerim, yPerim, zPerim] = ind2sub(size(imT), ind);
-%     
-%     unZ = unique(zPerim);
-%     
-%     xyCon = [];
-%     zCon = [];
-%    
-%     ptVal = 1:size(indexLine,1);
-%     
-%     for zH=1:length(unZ); 
-%         ind = find(zPerim ==unZ(zH));
-%         thisRow = cat(2, yPerim(ind), xPerim(ind));
-%         
-%         %See where this lies on the line
-%         t = ismember(indexLine, thisRow, 'rows');
-%         
-%         xyCon = [xyCon; ptVal(t)'];
-%         zCon  = [zCon; (1/0.1625)*zH*ones(length(ptVal(t)),1  )];
-%         
-%     end
-%     
-%     if(length(unique(zCon))>1)
-%        %Calculate the convex hull at this point along the line
-%        k = convhull(xyCon, zCon);
-%        xyCon = xyCon(k); zCon = zCon(k);
-%        convexPt{lineNum-1} = [xyCon, zCon];
-%     end
-%     
-% fprintf(2, '.');
-% 
-% end
-% 
-% fprintf('\n');
+
 
 end
--- OPAnalysis/tiffS.m
+++ OPAnalysis/tiffS.m
@@ -2,13 +2,34 @@
 
 
 
-function [] = tiffS(im)
+function [] = tiffS(varargin)
+
+%Where we'll save all the marker regions.
+saveDir = 'C:\jemielita\markers\';
+
+saveFile = saveDir;
+
+if nargin==1
+    im = mat2gray(varargin{1});
+    imPath = pwd;
+end
+
+if nargin==0
+    [imLoc, pathN] = uigetfile('.tif', 'Select the image stack to load in.');
+    imPath = [pathN imLoc];
+    imL  = imfinfo(imPath, 'tif');
+    
+    im = zeros(imL(1).Height, imL(2).Width, size(imL,1));
+    
+    for i=1:size(imL,1)
+        im(:,:,i) = imread(imPath, i);
+    end
+
+end
 
 im = mat2gray(im);
 
 h_fig = figure;
-
-
 set(h_fig,'KeyPressFcn',{@key_Callback,h_fig});
 
 minN = 1;
@@ -29,16 +50,7 @@ imT = im;
 hLine = imline(origAxes);
 pos = wait(hLine);
 
-
-
-%As a first pass let's see if a simple thresholding does the trick
-thresh = graythresh(im);
-imT = im>thresh;
-imT = double(imT);
-
-%imT = cleanup3dMarkers(imT);
-
-imT = bwlabeln(imT>0);
+imT = roughSegment(im);
 
 %Only keep the regions that intersect the line that we drew through the
 %center of the opercle.
@@ -48,13 +60,23 @@ yy = pos(1,2) + (1:1000)*(pos(2,2)-pos(1,2))/1000;
 
 b = 0;
 
-    function imOut = onlyOP(imT, xx, yy)
+    function imOut = roughSegment(imIn)
+        %As a first pass let's see if a simple thresholding does the trick
+        thresh = graythresh(imIn);
+        imOut = imIn>thresh;
+        imOut = double(imOut);
+        
+        %imT = cleanup3dMarkers(imT);
+        
+        imOut = bwlabeln(imOut>0);
+    end
+    function imOut = onlyOP(imIn, xx, yy)
         xx = round(xx);yy = round(yy);
         
-        imT2 = imT>0;
+        imT2 = imIn>0;
         
-        ind = sub2ind([size(imT,1), size(imT,2)],yy,xx);
-        temp = zeros(size(imT,1), size(imT,2));
+        ind = sub2ind([size(imIn,1), size(imIn,2)],yy,xx);
+        temp = zeros(size(imIn,1), size(imIn,2));
         temp(ind) = 1;
         
         temp = repmat(temp, [1,1,maxN]);
@@ -63,14 +85,14 @@ b = 0;
         
         ind = find(imT2(:)==2);
         
-        val = imT(ind);
+        val = imIn(ind);
         val = unique(val);
         
-        inter = ismember(imT, val);
+        inter = ismember(imIn, val);
         
-        imT(~inter) = 0;
+        imIn(~inter) = 0;
         
-        imOut = imT;
+        imOut = imIn;
         
     end
     function key_Callback(varargin)
@@ -105,8 +127,23 @@ b = 0;
                 end
                 
             case 's'
+                
+                posApi = iptgetapi(hLine);
+                pos = posApi.getPosition();
+                
+                xx = pos(1,1) + (1:1000)*(pos(2,1)-pos(1,1))/1000;
+                yy = pos(1,2) + (1:1000)*(pos(2,2)-pos(1,2))/1000;
+
+                
+                imT = roughSegment(imT);
                 imT = onlyOP(imT, xx, yy);
                 %Force the opercle to be the only region segmented.
+                
+                temp = segmentIm(im(:,:,index));
+                imOut = overlayIm(im(:,:,index), temp>0);
+                
+                set(segIm, 'CData', imOut);
+                
                 
             case 'm'
                 %measure the narrowest and widest part of the opercle
@@ -117,8 +154,126 @@ b = 0;
                 defaultanswer='';
                 answer=inputdlg(prompt,name,numlines,defaultanswer); 
                 b = 0;
-                
                
+            case 'c'
+                %Coursely segment the images
+                imT = roughSegment(im);
+               
+            case 'l'
+                %Load in a new set of images
+                nextIm = str2num(imPath(end-11:end-9));
+                nextIm = nextIm+1;
+                imPathNew = [imPath(1:end-12), sprintf('%03d', nextIm), imPath(end-8:end)];
+
+                %                 [imLoc, pathN] = uigetfile('.tif', 'Select the image stack to load in.',imPathNew);
+                %                 imPath = [pathN imLoc];
+                %                 imL  = imfinfo(imPath, 'tif');
+                
+                %Don't bother prompting the user-let's just wizz through
+                %these.
+                imPath  = imPathNew;
+                im = zeros(imL(1).Height, imL(2).Width, size(imL,1));
+                
+                imLoc = imPath(end-18:end);
+                
+                for i=1:size(imL,1)
+                    im(:,:,i) = imread(imPath, i);
+                end
+                
+                im = mat2gray(im);
+                
+                imT = im;
+                
+                set(hIm, 'CData', im(:,:,index));
+                set(origT, 'string', num2str(index));
+                
+                temp = segmentIm(im(:,:,index));
+                imOut = overlayIm(im(:,:,index), temp>0);
+                
+                set(segIm, 'CData', imOut);
+                
+                    
+                
+                
+            case 'd'
+                %Save markers made for this image
+                b= 0;
+                
+                outM = imLoc(1:end-4);
+                fn = [saveFile outM '.mat'];
+                evalC = ['save(' ,'''' , fn , ''' ,' ,' ''imT'' )'];
+                eval(evalC);
+                b = 0;
+                
+                
+           
+            case 'a'
+                thisIm = 47;
+                while(thisIm<145)
+                    thisIm = thisIm+1;
+                    %Segment all of these images!
+                    %(The results are somewhat rough after awhile anyway)
+                    %Load in a new set of images
+                    nextIm = str2num(imPath(end-11:end-9));
+                    nextIm = nextIm+1;
+                    imPathNew = [imPath(1:end-12), sprintf('%03d', nextIm), imPath(end-8:end)];
+                    
+                    %                 [imLoc, pathN] = uigetfile('.tif', 'Select the image stack to load in.',imPathNew);
+                    %                 imPath = [pathN imLoc];
+                    %                 imL  = imfinfo(imPath, 'tif');
+                    
+                    %Don't bother prompting the user-let's just wizz through
+                    %these.
+                    imPath  = imPathNew;
+                    im = zeros(imL(1).Height, imL(2).Width, size(imL,1));
+                    
+                    imLoc = imPath(end-18:end);
+                    
+                    for i=1:size(imL,1)
+                        im(:,:,i) = imread(imPath, i);
+                    end
+                    
+                    im = mat2gray(im);
+                    
+                    imT = im;
+                    
+                    set(hIm, 'CData', im(:,:,index));
+                    set(origT, 'string', num2str(index));
+                    
+                    temp = segmentIm(im(:,:,index));
+                    imOut = overlayIm(im(:,:,index), temp>0);
+                    
+                    set(segIm, 'CData', imOut);
+                    
+                    
+                    %Then segment the images
+                    posApi = iptgetapi(hLine);
+                    pos = posApi.getPosition();
+                    
+                    xx = pos(1,1) + (1:1000)*(pos(2,1)-pos(1,1))/1000;
+                    yy = pos(1,2) + (1:1000)*(pos(2,2)-pos(1,2))/1000;
+                    
+                    
+                    imT = roughSegment(imT);
+                    imT = onlyOP(imT, xx, yy);
+                    %Force the opercle to be the only region segmented.
+                    
+                    temp = segmentIm(im(:,:,index));
+                    imOut = overlayIm(im(:,:,index), temp>0);
+                    
+                    set(segIm, 'CData', imOut);
+                    
+                    %And save the result
+                    outM = imLoc(1:end-4);
+                    fn = [saveFile outM '.mat'];
+                    evalC = ['save(' ,'''' , fn , ''' ,' ,' ''imT'' )'];
+                    eval(evalC);
+                    b = 0;
+                    
+                    
+                end
+                
+                
         end
         
         
