526ddf94f8fe76c9cba7eeafb1c081e1981d92d7
lsm_Analysis_mlj <mlj@uoregon.edu>
2012-05-14 15:06:28 -0700

    Adding opercle code.

---
+++ OPAnalysis/multipleHulls.m
@@ -0,0 +1,28 @@
+
+%Multiple convex hulls
+
+function [] = multipleHulls(fileDir, sMin, sMax, type)
+plotData = 'true';
+for i=sMin:sMax
+    
+    if(type==1)
+        fN = [fileDir,filesep, 'OP_Scan', sprintf('%03d', i), '.mat'];
+    else
+        
+        fN = [fileDir, filesep, 'an', sprintf('%03d', i), '_cmle.mat'];
+    end
+    %     imT = load(fN);
+    %     imT = imT.mO-imT.mD;
+    %     imT = imT>0;
+    
+    imT = load(fN);
+    imT = imT.imT;
+    [convexPt, linePt] = opWidth(imT,i);
+    
+    if(strcmp(plotData, 'false'))
+        conF = [fileDir,filesep,'OP_Scan', sprintf('%03d', i), 'convex.mat'];
+        save(conF, 'convexPt', 'linePt');
+    end
+end
+
+end
---
+++ OPAnalysis/opercleRatio.m
@@ -0,0 +1,34 @@
+function val = opercleRatio
+
+minS = 1;
+maxS = 143;
+
+val = zeros(maxS-minS, 2);
+
+for i=minS:maxS
+        convex = load(['OP_Scan', sprintf('%03d', i), 'convex.mat']);
+  %  outline = load(['OP_Scan', sprintf('%03d', i), 'perim.mat']);
+    
+
+  try
+      perim = convex.perim;
+    
+    
+    sP = length(perim);
+    maxVal = max(perim(1:floor(sP/2)));
+    
+    %Find the min value later 
+    index = find(perim==maxVal);
+    minVal = min(perim(index+1:floor(2*sP/3)));
+
+
+    val(i, 1) = maxVal;
+    val(i,2) = minVal;
+    catch
+        val(i,1) = -1;
+        val(i,2) = -1;
+    end
+        
+end
+
+end
