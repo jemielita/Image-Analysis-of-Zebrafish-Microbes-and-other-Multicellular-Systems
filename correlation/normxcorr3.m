fa24a27dc1be61f8992fb3dcf681f153e6f173c1
Matthew Jemielita <MatthewLJemielita@gmail.com>
2013-02-15 11:48:48 -0800

    Changed normxcorr3 to make normalization optional.

--- correlation/normxcorr3.m
+++ correlation/normxcorr3.m
@@ -1,4 +1,4 @@
-function C = normxcorr3(T, A, shape)
+function C = normxcorr3(T, A, shape, normalizeCorr)
 % C = normxcorr3(TEMPLATE, IMAGE, SHAPE)
 %
 %       TEMPLATE - type double, ndims==3, size <= size of image
@@ -7,6 +7,10 @@ function C = normxcorr3(T, A, shape)
 %                  'full' by default
 %
 %       C        - values in [-1,1]. size depends on SHAPE
+%
+%       normalizeCorr: true (default) normalize the cross correlation. This
+%       is the standard form for cross correlation. False: don't apply
+%       normalization. Optional input.
 %
 % the syntax of this function is identical to Matlab's
 % normxcorr2, except that it's been extended to 3D matrices,
@@ -22,6 +26,10 @@ function C = normxcorr3(T, A, shape)
 
 if nargin<3
 	shape = 'full';
+end
+
+if nargin <4
+    normalizeCorr = false;
 end
 
 if ndims(A)~=3 || ndims(T)~=3
@@ -54,8 +62,16 @@ fftA = fftn(A,szOut);
 corrTA = real(ifftn(fftA.*fftRotT));
 num = (corrTA - intImgA*sum(T(:))/pSzT ) / (pSzT-1);
 
-% compute the denominator of the NCC
-denomA = sqrt( ( intImgA2 - (intImgA.^2)/pSzT ) / (pSzT-1) );
+
+
+if(normalizeCorr==true
+    % compute the denominator of the NCC
+    denomA = sqrt( ( intImgA2 - (intImgA.^2)/pSzT ) / (pSzT-1) );
+else
+    %Don't normalize the correlation function.
+    denomA = 1;
+end
+
 denomT = std(T(:));
 denom = denomT*denomA;
 
