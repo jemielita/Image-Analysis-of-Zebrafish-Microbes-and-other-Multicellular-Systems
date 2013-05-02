416add9a4d3b43a4e122b334a952e3dcfb0656ba
Matthew Jemielita <MatthewLJemielita@gmail.com>
2013-05-02 12:37:08 -0700

    Merging code.

--- segmentation/combineRegions.m
+++ segmentation/combineRegions.m
@@ -5,8 +5,12 @@ function spotLoc = combineRegions(spotLoc)
 maxNumReg = length(spotLoc);
 radCutoffH =40; %Combine together all regions within radCutoff microns of each other
 radCutoffV = 3; %Vertical distance cutoff
+
+radCutoff = 10;
+
 loc = [spotLoc(:).Centroid];
 loc = reshape(loc, 3,length(spotLoc));
+
 loc(1:2,:) = (0.1625)*loc(1:2,:); %Resize in z direction
 
 inten = [spotLoc(:).MeanIntensity];
@@ -19,15 +23,34 @@ val = [inten; loc];
 distH = dist(val(2:3,:));
 distV = dist(val(4,:));
 
+distA = dist(val(2:4,:));
+
 remInd = []; %Regions to cull out
+
+
+%Get a ~ radius of each region by finding the maximum width of the bounding
+%box
+
 for nR=1:maxNumReg
-    thisDV = distV(:,nR);
-    thisDH = distH(:,nR);
-    closeD = find(thisDH<radCutoffH |thisDV<radCutoffV); closeD(closeD==nR) = [];
-    if(prod(val(1,nR)<val(1,closeD))==0)
+    %Rescale bounding box
+    spotLoc(nR).BoundingBox(4:5) = (0.1625)*spotLoc(nR).BoundingBox(4:5);
+   spotLoc(nR).EffRadius =  max([spotLoc(nR).BoundingBox(4:6)])/2;
+end
+
+for nR=1:maxNumReg
+    
+ %   thisD = distA(:,nR)-spotLoc(nR).EffRadius-[spotLoc(nR).EffRadius];
+  thisD = distA(:,nR);
+  closeD = find(thisD<radCutoff); closeD(closeD==nR) = [];
+    %    thisDV = distV(:,nR);
+    %   thisDH = distH(:,nR);
+    %  closeD = find(thisDH<radCutoffH |thisDV<radCutoffV); closeD(closeD==nR) = [];
+   
+  
+  if(prod(val(1,nR)<val(1,closeD))==0)
         remInd = [remInd, nR];
     end 
 end
 
-spotLoc(remInd)= [];
+spotLoc(remInd) = [];
 end
--- segmentation/countSingleBacteria.m
+++ segmentation/countSingleBacteria.m
@@ -23,12 +23,14 @@ maskAll = zeros(size(im), 'uint8');
 % Filter image using wavelet filter
 fprintf(1, 'Filtering image and segmenting.');
 for nZ=1:size(im,3)
-   [mask,thisFrame] = spotDetector(double(im(:,:,nZ)));
-   mask = uint8(mask);
-   mask(thisFrame>maxThresh) =1;
-   mask(thisFrame>minThresh) = mask(thisFrame>minThresh)+1;
+   [~,thisFrame] = spotDetector(double(im(:,:,nZ)));
    
-   maskAll(:,:,nZ)= mask;
+   
+   %mask = uint8(mask);
+   %mask(thisFrame>maxThresh) =1;
+   %mask(thisFrame>minThresh) = mask(thisFrame>minThresh)+1;
+   
+  % maskAll(:,:,nZ)= mask;
    imSeg(:,:,nZ) = thisFrame;
   % imSeg(:,:,nZ)= mask;
    %Processing this z-slice
@@ -56,44 +58,54 @@ for nZ=1:size(im,3)
 end
 fprintf(1, '\n');
 
-spotLoc = regionprops(imSeg>200, imSeg, 'Centroid', 'Area', 'MeanIntensity', 'BoundingBox');
-spotLoc([spotLoc.Area]<500)  = [];
+spotLoc = regionprops(imSeg>maxThresh, imSeg, 'Centroid', 'Area', 'MeanIntensity', 'BoundingBox');
+
+
+%spotLoc([spotLoc.Area]<500)  = [];
 % Linking together regions
-fprintf(1, 'Linking together 3d regions:');
-numR = sum(imSeg(:));
-numRprev = 0;
-while(numRprev ~= numR)
-
-    for i = 2:size(im,3)-1
-        [r,c] = find(imSeg(:,:,i-1)+imSeg(:,:,i+1)>2);
-        
-        %Find regions that overlap with
-        thisPlane = imSeg(:,:,i);
-        bw = bwselect(thisPlane,c,r,4);
-        thisPlane(bw) = 2;
-        imSeg(:,:,i) = thisPlane;       
-    end
-    
-    numRprev = numR;
-    numR = sum(imSeg(:));
-    fprintf(1, '.');
-end
-fprintf(1, 'done!\n');
-
-%Get properties of each of the identified bacteria
-bw = imSeg==2;
-spotLoc = regionprops(bw, im, 'Centroid', 'Area', 'MeanIntensity', 'BoundingBox');
 
 %Calculate eccentricity from 3d projections
-labelM = bwlabeln(bw);
-uniqL = unique(labelM(labelM>0));
-for nL = 1:length(uniqL)
-   thisL = uniqL(nL);
-   maxL = max(labelM==thisL,[],3);
-   ecc = regionprops(maxL, 'Eccentricity');
-   spotLoc(thisL).Eccentricity = ecc.Eccentricity;
-end
+% bw = imSeg>maxThresh;
+% labelM = bwlabeln(bw);
+% uniqL = unique(labelM(labelM>0));
+% for nL = 1:length(uniqL)
+%    thisL = uniqL(nL);
+%    maxL = max(labelM==thisL,[],3);
+%    ecc = regionprops(maxL, 'Eccentricity');
+%    spotLoc(thisL).Eccentricity = ecc.Eccentricity;
+% end
 
+
+
+
+% 
+% 
+% 
+% 
+% fprintf(1, 'Linking together 3d regions:');
+% numR = sum(imSeg(:));
+% numRprev = 0;
+% while(numRprev ~= numR)
+% 
+%     for i = 2:size(im,3)-1
+%         [r,c] = find(imSeg(:,:,i-1)+imSeg(:,:,i+1)>2);
+%         
+%         %Find regions that overlap with
+%         thisPlane = imSeg(:,:,i);
+%         bw = bwselect(thisPlane,c,r,4);
+%         thisPlane(bw) = 2;
+%         imSeg(:,:,i) = thisPlane;       
+%     end
+%     
+%     numRprev = numR;
+%     numR = sum(imSeg(:));
+%     fprintf(1, '.');
+% end
+% fprintf(1, 'done!\n');
+% 
+% %Get properties of each of the identified bacteria
+% bw = imSeg==2;
+% spotLoc = regionprops(bw, im, 'Centroid', 'Area', 'MeanIntensity', 'BoundingBox');
 
 
 end
