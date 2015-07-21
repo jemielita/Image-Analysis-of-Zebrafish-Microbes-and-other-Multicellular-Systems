1ad0cef72df96d69977b0a025638c849bb6b17da
Matthew Jemielita <mlj@uoregon.edu>
2015-07-21 16:22:52 -0700

    Adding code to check image number consistency.

--- registerImages/saveCroppedBatch.m
+++ registerImages/saveCroppedBatch.m
@@ -74,38 +74,12 @@ totalNumScans = param.expData.totalNumberScans;
 totalNumColors = size(param.color,2);
 
 
-%Check to make sure that every scan has the same number of images in it
-    for nS=1:totalNumScans
-        for nR = 1:totalNumRegions
-            for nC = 1:totalNumColors
-                dirName = [param.directoryName filesep 'Scans' filesep 'scan_', num2str(nS), filesep ...
-                    'region_', num2str(nR), filesep param.color{nC}];
-                
-                imComp{nR}(nS,nC) = length(ls(dirName));
-                
-            end
-        end
-    end
-    checkSz = cellfun(@(x)length(unique(x)), imComp);
-    if(unique(checkSz)==1)
-        fprintf(1, 'All scans have the same number of images!\n');
-       
-    else
-        fprintf(2, 'Scans contains a different number of images in each scan! Fix!\n')
-        %Output list of scans so the user can check things:
-        
-        for nR = 1:totalNumRegions
-            for nS=1:totalNumScans
-                    for nC=1:totalNumColors
-                       numIm(nC) = imComp{nR}(nS, nC);
-                    end
-                    fprintf(1, ['Region: ', num2str(nR) 'Scan ' num2str(nS), ' # images:  ' num2str(numIm) , '\n']);
-                    
-            end
-        end
-        
-        return
-    end
+%Check to make sure that the directories all have the same number of images
+err = checkScanImageNumber(totalNumScans, totalNumRegions, totalNumColors, param);
+if(err==1)
+    return;
+end
+
     
 %Auto crop down if desired.
 if(minCrop==true)
