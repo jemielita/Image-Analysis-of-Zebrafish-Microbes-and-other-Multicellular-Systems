12dcd4195c4d0d3af32aab1927eb99714ba0cacc
Matthew Jemielita <bschloma@uoregon.edu>
2015-09-02 14:49:26 -0700

    Routines added for updating total number of scans in param and fish files

--- classes/clumpSClass.m
+++ classes/clumpSClass.m
@@ -246,7 +246,7 @@ classdef clumpSClass
                 inputVar = load(fileName);
                 c = inputVar.c;
                 
-                c = c.calculateSliceInten(obj.scanNum);
+                c = c.calculateSliceInten;
                 c.save; 
                 fprintf(1, '.');
             end
